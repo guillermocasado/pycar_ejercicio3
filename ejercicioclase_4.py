b070aa154c3a6bc956b6ba14e1333e9c39cd02fd
Guillermocasado <gcasadom2@alumnos.nebrija.es>
2021-10-14 15:04:55 +0200

    cambio de color del fondo

---
+++ ejercicioclase_4.py
@@ -0,0 +1,6 @@
+#comandos usados en el ejercicio 4
+#en el main se pone junto a size el titulo
+#en main.fuctions se declara la variable titulo. Esta se añade a pygame.display para que aparezca de titulo
+#en la ventana emergente
+
+#variable tipo tuple es entre parentesis
--- main.py
+++ main.py
@@ -7,4 +7,9 @@ pygame.init()
 
 #call main routine
 size = [1300, 600] # Define size of windows
-main2(size)
+titulo = input('Dime el nombre del titulo:')
+red = int(input('Dame la intensidad del rojo:'))
+green = int(input('Dame la intensidad del verde:'))
+blue = int(input('Dame la intensidad del azul:'))
+colores = (red, green, blue) #variable con los tres colores
+main2(size = size, titulo = titulo, colores = colores) #el = sirve para que no importe el orden en el que se introducen
